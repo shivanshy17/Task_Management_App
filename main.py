154bbb334efdd82a44a2fd75f71c2e508fc7c4ad
ishivansh2717 <146498986+shivanshy17@users.noreply.github.com>
2026-05-15 22:58:17 +0530

    Add comments for task management application functionality
    Added comments to describe the functionality of the application.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from fastapi import FastAPI
 
 app=FastAPI(title="This is my Task Management Application")
+
+# Posting the management task, updating the task/task progress, deleting the task when completed, real time track on the assets/time.
